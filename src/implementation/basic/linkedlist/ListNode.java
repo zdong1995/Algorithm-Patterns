4c458be63417425ee87634994f79522837d0cb77
Zhang Dong <zhangdong.1995@outlook.com>
2020-11-11 23:58:52 -0500

    Start HashMap and extract ListNode

---
+++ src/implementation/basic/linkedlist/ListNode.java
@@ -0,0 +1,11 @@
+package implementation.basic.linkedlist;
+
+public class ListNode {
+  int val;
+  ListNode next;
+
+  public ListNode(int val) {
+    this.val = val;
+    this.next = null;
+  }
+}
