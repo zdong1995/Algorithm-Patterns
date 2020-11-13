baa2a7d60469a9008f281c1656d109998bbb5848
Zhang Dong <zhangdong.1995@outlook.com>
2020-11-12 23:26:35 -0500

    PathSum

---
+++ src/algorithm/tree/pathsum/MaxPathSumNodeToNode.java
@@ -0,0 +1,23 @@
+package algorithm.tree.pathsum;
+
+import algorithm.tree.TreeNode;
+
+public class MaxPathSumNodeToNode {
+  // sum = the largest sum path that ends at root
+  private void helper(TreeNode root, int[] max, int sum) {
+    // base case
+    if (root == null) {
+      return;
+    }
+
+    if(sum < 0) {
+      sum = root.val;
+    } else {
+      sum += root.val;;
+    }
+    max[0] = Math.max(max[0], sum); // pre-order traversal
+
+    helper(root.left, max, sum);
+    helper(root.right, max, sum);
+  }
+}
