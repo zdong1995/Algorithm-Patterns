4cb728e186292b089a3245c1780f480169bc7fc0
Zhang Dong <zhangdong.1995@outlook.com>
2020-11-14 23:58:19 -0500

    2sum

---
+++ src/algorithm/tree/twosum/TwoSum.java
@@ -0,0 +1,21 @@
+package algorithm.tree.twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+  public int[] twoSum(int[] array, int target) {
+    if (array == null || array.length == 0) {
+      return new int[] {-1, -1};
+    }
+    Map<Integer, Integer> seen = new HashMap<>(); // <value, index>
+    for (int i = 0; i < array.length; i++) {
+      if (seen.containsKey(target - array[i])) {
+        return new int[] {i, seen.get(target - array[i])};
+      } else {
+        seen.put(array[i], i);
+      }
+    }
+    return new int[] {-1, -1};
+  }
+}
